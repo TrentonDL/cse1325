2a77fa106947c26b8920611639977dbcc00eb635
TrentonDL <trenton.laule@mavs.uta.edu>
2023-01-18 17:11:12 -0600

    using try to close in

--- hello.java
+++ hello.java
@@ -1,10 +1,13 @@
 import java.util.Scanner;
 
 public class hello {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.print("Enter your Name: ");
-        String name = in.nextLine();
-        System.out.println("Hello "+ name + "!");
+    public static void main(String[] args) 
+    {
+        try (Scanner in = new Scanner(System.in)) 
+        {
+            System.out.print("Enter your Name: ");
+            String name = in.nextLine();
+            System.out.println("Hello "+ name + "!");
+        }
     }
 }
