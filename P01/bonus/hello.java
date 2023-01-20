b93439a9eeb91331edc581d4267dc365b1967ccc
TrentonDL <trenton.laule@mavs.uta.edu>
2023-01-20 17:40:50 -0600

    Update hello.java

--- P01/bonus/hello.java
+++ P01/bonus/hello.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class hello {
     public static void main(String[] args) {
         try (Scanner in = new Scanner(System.in)) {
-            System.out.print("Enter your Name: ");
+            System.out.println("Enter your Name: ");
             String name = in.nextLine();
             System.out.println("Hello " + name + "!");
         }
