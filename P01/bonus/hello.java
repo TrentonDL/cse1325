b0bed0e0b9152661f8cf29a4f0831d273a04932d
TrentonDL <trenton.laule@mavs.uta.edu>
2023-01-23 11:10:27 -0600

    using in.close() instead of try

--- P01/bonus/hello.java
+++ P01/bonus/hello.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 
 public class hello {
     public static void main(String[] args) {
-        try (Scanner in = new Scanner(System.in)) {
-            System.out.println("Enter your Name: ");
-            String name = in.nextLine();
-            System.out.println("Hello " + name + "!");
-        }
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter your Name: ");
+        String name = in.nextLine();
+        System.out.println("Hello " + name + "!");
+        in.close();
     }
 }
