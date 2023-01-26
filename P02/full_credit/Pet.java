585b4a48155037ac4bddaefa5cc85dbb36ea84a2
Trenton Laule <trenton.laule@mavs.uta.edu>
2023-01-26 13:42:40 -0600

    starting Pet.java

--- P02/full_credit/Pet.java
+++ P02/full_credit/Pet.java
@@ -1,3 +1,17 @@
+import java.lang.ProcessBuilder.Redirect.Type;
+
 public class Pet {
-    
+    @Override
+    public String toString(){
+        return name + " is a " + type + " age " + age;
+    }
+
+    public Double humanAge() {
+        this.age = humanLifeSpan;
+    }
+
+    private String name;
+    private Type type;
+    private Double age;
+    private static final Double humanLifeSpan = 80.0;
 }
