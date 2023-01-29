07ee38581493acd03b330e676735780f37318915
TrentonDL <trenton.laule@mavs.uta.edu>
2023-01-28 23:31:24 -0600

    trying to figure out adding pets to the array

--- P02/full_credit/MyPets.java
+++ P02/full_credit/MyPets.java
@@ -1,10 +1,19 @@
+import java.util.Arrays;
+
 public class MyPets {
     public static void main(String[] args) {
+        int p = 0;
         Pet Kevin = new Pet("Kevin", 4.0, Type.Snake);
+        ++p;
         Pet Jack = new Pet("Jack", 15.0, Type.Alligator);
+        ++p;
         Pet Lucy = new Pet("Lucy", 12.0, Type.Lizard);
+        ++p;
         Pet Leonardo = new Pet("Leonardo", 15.0, Type.Turtle);
+        ++p;
 
-        
+        for (Pet iterable_element : iterable) {
+
+        }
     }
 }
