bf2ae6b7b2a398b874ec50e1534120880878aef5
TrentonDL <trenton.laule@mavs.uta.edu>
2023-01-30 13:17:54 -0600

    putting my pets into an array

--- P02/full_credit/MyPets.java
+++ P02/full_credit/MyPets.java
@@ -2,18 +2,15 @@ import java.util.Arrays;
 
 public class MyPets {
     public static void main(String[] args) {
-        int p = 0;
-        Pet Kevin = new Pet("Kevin", 4.0, Type.Snake);
-        ++p;
-        Pet Jack = new Pet("Jack", 15.0, Type.Alligator);
-        ++p;
-        Pet Lucy = new Pet("Lucy", 12.0, Type.Lizard);
-        ++p;
-        Pet Leonardo = new Pet("Leonardo", 15.0, Type.Turtle);
-        ++p;
+        Pet[] pets = new Pet[20];
 
-        for (Pet iterable_element : iterable) {
+        pets[0] = new Pet("Kevin", 4.0, Type.Snake);
+        pets[1] = new Pet("Jack", 15.0, Type.Alligator);
+        pets[2] = new Pet("Lucy", 12.0, Type.Lizard);
+        pets[3] = new Pet("Leonardo", 15.0, Type.Turtle);
 
+        for (int element : pets) {
+            System.out.println(pets[element]);
         }
     }
 }
