fb17fd2fc5864f4a16f2d10d3ecbeed04c049f29
TrentonDL <trenton.laule@mavs.uta.edu>
2023-02-02 15:25:44 -0600

    starting on question.java

--- P03/full_credit/Question.java
+++ P03/full_credit/Question.java
@@ -1,3 +1,28 @@
+import java.util.ArrayList;
+
 public class Question {
-    
+    public Question(String question, ArrayList<String> answers, int rightAnswer){
+        this.question = question;
+        this.answers = answers;
+        this.rightAnswer = rightAnswer;
+    }
+
+    public Boolean checkAnswer(int answer){
+        this.answer = answer;
+    }
+
+    @Override
+    public String toString(){
+        return nextQuestionNumber + ". " + question + "\n";
+    }
+
+    private String question;
+
+    private ArrayList<String> answers; 
+
+    private int rightAnswer;
+
+    private int nextQuestionNumber = 1;
+
+    private int final questionNUmber;
 }
