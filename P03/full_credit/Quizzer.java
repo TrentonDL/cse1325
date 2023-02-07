8caafe282fc918080b71208d19e3bc8b2f0e8e79
Trenton Laule <trenton.laule@mavs.uta.edu>
2023-02-07 09:57:48 -0600

    using getMessage() instead

--- P03/full_credit/Quizzer.java
+++ P03/full_credit/Quizzer.java
@@ -1,13 +1,13 @@
 public class Quizzer {
     public static void main(String[] args) {
-        try{
+        try {
             Quiz resultQuiz = new Quiz();
             double grade = resultQuiz.takeQuiz();
-    
+
             System.out.println("Your grade : " + grade);
-        }catch (IllegalArgumentException i){
-            System.err.println("Invaild Answer Choice");
+        } catch (IllegalArgumentException i) {
+            System.err.println(i.getMessage() + "\nQuiz Aborted");
             System.exit(-1);
-        } 
+        }
     }
 }
