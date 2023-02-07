cccc02505fe74e2aa000216337b9deaeba732e8d
Trenton Laule <trenton.laule@mavs.uta.edu>
2023-02-06 18:07:52 -0600

    working on Quiz.java

--- P03/full_credit/Quiz.java
+++ P03/full_credit/Quiz.java
@@ -1,3 +1,19 @@
+import java.util.ArrayList;
+
 public class Quiz {
-    
+    private class question {
+        loadQuiz();
+    }
+
+    private void loadQuiz(){
+        ArrayList<Question> question = new ArrayList<>();
+        ArrayList<String> answers1 = new ArrayList<>();
+        answers1.add(1, "Dog");
+        answers1.add(2, "Snake");
+        answers1.add(3,"Dragon");
+        answers1.add(4,"Spider");
+
+        Question.Quiz("Which animal is not real?", );
+        Question.add("Who was the founder of Microsoft?");
+    }
 }
