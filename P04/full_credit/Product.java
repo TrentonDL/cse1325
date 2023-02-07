08c2873de62194003c0a960a2b24f04c226962ef
Trenton Laule <trenton.laule@mavs.uta.edu>
2023-02-07 10:45:32 -0600

    adding P04

---
+++ P04/full_credit/Product.java
@@ -0,0 +1,18 @@
+abstract class Product {
+    public Product(String name, double cost){
+        this.name = name;
+        this.cost = cost;
+    }
+    abstract double price(){
+
+    } 
+    
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return super.toString();
+    }
+
+    protected double cost;
+    protected String name;
+}
