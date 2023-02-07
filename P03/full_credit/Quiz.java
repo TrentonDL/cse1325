35df66b92bb417d09f80fe4d67c09949eb39ff6f
Trenton Laule <trenton.laule@mavs.uta.edu>
2023-02-06 18:32:03 -0600

    got the question array sorted out -ish

--- P03/full_credit/Quiz.java
+++ P03/full_credit/Quiz.java
@@ -2,18 +2,25 @@ import java.util.ArrayList;
 
 public class Quiz {
     private class question {
-        loadQuiz();
+        Quiz.loadQuiz();
     }
 
     private void loadQuiz(){
-        ArrayList<Question> question = new ArrayList<>();
+        Question[] question;
+    
         ArrayList<String> answers1 = new ArrayList<>();
         answers1.add(1, "Dog");
         answers1.add(2, "Snake");
         answers1.add(3,"Dragon");
         answers1.add(4,"Spider");
-
-        Question.Quiz("Which animal is not real?", );
-        Question.add("Who was the founder of Microsoft?");
+        question[0] = new Question("Which animal is not real?", answers1, 3);
+        
+        ArrayList<String> answers2 = new ArrayList<>();
+        answers1.add(1, "Bill Gates");
+        answers1.add(2, "Jeff Bazos");
+        answers1.add(3,"Mark Cuban");
+        answers1.add(4,"Joe Rogan");
+        question[1] = new Question("Which animal is not real?", answers2, 1);
+       
     }
 }
