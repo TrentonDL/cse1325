1b0806969acba51372f866a38ca0131387860ff3
Trenton Laule <trenton.laule@mavs.uta.edu>
2023-02-06 21:18:59 -0600

    instance questions[i].checkAnswer()

--- P03/full_credit/Quiz.java
+++ P03/full_credit/Quiz.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Quiz {
-    private Question[] question;
+    private Question[] questions;
 
     private void loadQuiz(){
         ArrayList<String> answers1 = new ArrayList<>();
@@ -10,14 +10,15 @@ public class Quiz {
         answers1.add(2, "Snake");
         answers1.add(3,"Dragon");
         answers1.add(4,"Spider");
-        question[0] = new Question("Which animal is not real?", answers1, 3);
+        questions[0] = new Question("Which animal is not real?", answers1, 3);
         
         ArrayList<String> answers2 = new ArrayList<>();
         answers2.add(1, "Bill Gates");
         answers2.add(2, "Jeff Bazos");
         answers2.add(3,"Mark Cuban");
         answers2.add(4,"Joe Rogan");
-        question[1] = new Question("Which animal is not real?", answers2, 1);
+        questions[1] = new Question("Which animal is not real?", answers2, 1);
+        questions[i].checkAnswer(0);
     }
 
     public Double takeQuiz(){
@@ -30,8 +31,6 @@ public class Quiz {
         System.out.printf("\nEnter your answer: ");
         in.nextInt(proposedAnswer);
         in.close();
-
-        Question 
 
         if(Question.checkAnswer(proposedAnswer)){
             ++correctAnswer;
