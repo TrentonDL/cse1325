a6b4bb3eaee32e6088b2beecf98149f1479addda
Trenton Laule <trenton.laule@mavs.uta.edu>
2023-02-06 20:43:57 -0600

    still working on it slowly but surely

--- P03/full_credit/Quiz.java
+++ P03/full_credit/Quiz.java
@@ -1,9 +1,8 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Quiz {
-    private class question {
-        Quiz.loadQuiz();
-    }
+    private Question[] question;
 
     private void loadQuiz(){
         Question[] question;
@@ -16,11 +15,31 @@ public class Quiz {
         question[0] = new Question("Which animal is not real?", answers1, 3);
         
         ArrayList<String> answers2 = new ArrayList<>();
-        answers1.add(1, "Bill Gates");
-        answers1.add(2, "Jeff Bazos");
-        answers1.add(3,"Mark Cuban");
-        answers1.add(4,"Joe Rogan");
+        answers2.add(1, "Bill Gates");
+        answers2.add(2, "Jeff Bazos");
+        answers2.add(3,"Mark Cuban");
+        answers2.add(4,"Joe Rogan");
         question[1] = new Question("Which animal is not real?", answers2, 1);
        
+    }
+
+    public Double takeQuiz(){
+        loadQuiz();
+        int proposedAnswer = 0;
+        Double correctAnswer = 0.0;
+        Double totalQuestions = 0.0;
+
+        Scanner in = new Scanner(System.in);
+        System.out.printf("\nEnter your answer: ");
+        in.nextInt(proposedAnswer);
+        in.close();
+        if(Question.checkAnswer(proposedAnswer)){
+            ++correctAnswer;
+            ++totalQuestions;
+        }
+        else{
+            ++totalQuestions;
+        }
+        return (correctAnswer/totalQuestions);
     }
 }
--- P03/full_credit/Quizzer.java
+++ P03/full_credit/Quizzer.java
@@ -1,5 +1,5 @@
 public class Quizzer {
     public static void main(String[] args) {
-        
+        Quiz.takeQuiz();
     }
 }
