e4e112adf823bfb20941af172e0647470a792096
Trenton Laule <trenton.laule@mavs.uta.edu>
2023-02-24 14:59:02 -0600

    Data vaildation and toString added

--- P05/Customer.java
+++ P05/Customer.java
@@ -3,9 +3,19 @@ public class Customer {
         this.name = name;
         this.email = email;
 
-        if(email.equals(null) || !(email.contentEquals(".com"))){
-
+        if((!(email.contains("@")) && !(email.contains(".")))){
+            throw new IllegalArgumentException("Error: Invaild Email Address");
         }
+    }
+
+    @Override
+    public String toString(){
+        return name + "(" + email + ")";
+    }
+
+    @Overide
+    public boolean equals(){
+        
     }
 
     private String name;
