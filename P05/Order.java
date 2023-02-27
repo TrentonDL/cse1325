f8d041d54da86c4f36b70f4c9a8507d72e8d1f5e
TrentonDL <trenton.laule@mavs.uta.edu>
2023-02-26 22:32:18 -0600

    wrote order.java

--- P05/Order.java
+++ P05/Order.java
@@ -1,6 +1,23 @@
 import java.util.ArrayList;
 
 public class Order {
+    public Order(Customer customer){
+        this.customer = customer;
+    }
+
+    public void addComputer(Computer computer){
+        computers.add(new Computer(computer));
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder s = new StringBuilder("Order " + orderNumber++ + " for " + customer.toString() + "\n\n");
+        for (Computer computer : computers) {
+            s.append(computer.toString());
+        } 
+        return s.toString();
+    }
+
     private static long nextOrderNumber = 0;
     private long orderNumber;
     private Customer customer;
