b3f142999f50a58c466c4a71353aa495cfa54b92
TrentonDL <trenton.laule@mavs.uta.edu>
2023-02-27 22:59:15 -0600

    spaceing changes

--- P05/Order.java
+++ P05/Order.java
@@ -1,30 +1,30 @@
 import java.util.ArrayList;
 
 public class Order {
-    public Order(Customer customer){
+    public Order(Customer customer) {
         this.customer = customer;
         this.orderNumber = nextOrderNumber++;
         this.computers = new ArrayList<>();
     }
 
-    public void addComputer(Computer computer){
+    public void addComputer(Computer computer) {
         this.computers.add(computer);
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         StringBuilder s = new StringBuilder("Order " + orderNumber++ + " for " + customer.toString() + "\n\n");
         for (Computer computer : computers) {
             s.append(computer.toString());
-        } 
+        }
         return s.toString();
     }
 
     @Override
-    public boolean equals(Object o){
-        if(this == o)
+    public boolean equals(Object o) {
+        if (this == o)
             return true;
-        if(o == null || getClass() != o.getClass()) 
+        if (o == null || getClass() != o.getClass())
             return false;
         final Order that = (Order) o;
         if (!this.customer.equals(that.customer)) {
