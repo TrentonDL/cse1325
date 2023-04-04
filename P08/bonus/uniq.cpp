451b464f97c51c8994fe80d864f7bf4180e935d1
Trenton Laule <trenton.laule@mavs.uta.edu>
2023-04-03 22:17:03 -0500

    welp i tried the bonus having trouble with it printing so idk whats wrong

--- P08/bonus/uniq.cpp
+++ P08/bonus/uniq.cpp
@@ -1,26 +1,24 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+using namespace std;
 
 int main(int args, char* argv[]){
-    std::string s;
-    std::stringstream ss;
-    std::string s1;
-    std::string s2;
+    string s;
+    string s1;
+    string s2;
     int count = 0;
     s2 = argv[count];
 
-    while(s2.compare(NULL) == 0){
+    while(s2.empty()){
         if(s2.compare(s1) == 1)
             s2 = argv[++count];
         else if(s2.compare(s1) == 0){
-            ss << s2;
+            s.append(s2 + " ");
             s1 = s2;
             s2 = argv[++count];
         }
     }
 
-    ss >> s;
-    std::cout << s;
-    
+    cout << s << endl;
 }
